55ebce6d267953c0b620520edf4a7d7467bd2109
Leskhan Karatayev <leskhan001@gmail.com>
2019-05-25 02:16:39 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+
+</style>
